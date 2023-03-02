4f72fc14b60e74d64947f86a2f5f084488fd3a07
kou <contact@awesome-linus.com>
2023-03-02 19:12:34 +0900

    add iframe

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-const elem = document.getElementById('hoge');
-elem!.style.color = "red";
+function showIframe(elementId: string) {
+    const iframe = document.createElement("iframe");
+    iframe.src = "https://www.youtube.com/embed/WPK50Qtz2Z0";
+    iframe.style.width = "400px";
+    iframe.style.height = "400px";
+    document.getElementById(elementId)?.appendChild(iframe);
+}
